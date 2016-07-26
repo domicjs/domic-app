dd02d74dff6880278fba930a58ea6377d5a4f1d2
Christophe Eymard <christophe.eymard@sales-way.com>
2016-07-26 17:21:45 +0200

    lots of evolutions of structure

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
 
 import {Eventable, resolve, pathget, Atom} from 'carbyne'
-
-export class Views<P extends Partial> {
-
-  [name: string]: (part: P) => Atom
-
-}
+import * as Reflect from 'reflect-metadata'
 
 
 export type Params = {
@@ -14,86 +9,116 @@ export type Params = {
 
 }
 
-export interface Constructor<P extends Partial> {
-  new(...a: any[]): P
+export type ComputedViews = {
+  [name: string]: () => Atom
 }
+
+export interface Constructor<S extends Service> {
+  new(...a: any[]): S
+}
+
 
 /**
  * Application
  */
 export class App extends Eventable {
 
-  protected active_partials: Map<Constructor<Partial>, Partial>
-  protected next_partials: Map<Constructor<Partial>, Partial> = null
+  public activating: boolean = false
 
-  protected registered_partials: {[name: string]: Constructor<Partial>}
+  // protected active_partials = new Map<Constructor<Partial>, Partial>()
+  // protected activating_partials: Map<Constructor<Partial>, Partial> = null
+  // protected currently_activating: Partial[] = []
+
+  /**
+   *
+   */
+  // protected registered_partials: {[name: string]: Constructor<Partial>}
 
   constructor() {
     super()
-    this.active_partials = new Map<Constructor<Partial>, Partial>()
+    // this.active_partials = new Map<Constructor<Partial>, Partial>()
+  }
+
+  block(): Block {
+    return new Block(this)
+  }
+
+  screen(): Screen {
+    return new Screen(this)
+  }
+
+  view<A extends Service, B extends Service>(c: Constructor<A>, b: Constructor<B>, fn: (a: A, b: B) => Atom): View;
+  view<A extends Service>(c: Constructor<A>, fn: (c: A) => Atom): View;
+  view(fn: () => Atom): View;
+  view(...a: any[]): View {
+    let v = new View(this)
+    return v
+  }
+
+  /**
+   * Get the views as computed from the partials.
+   */
+  getViews(): ComputedViews {
+    let result: ComputedViews = {}
+
+    // this.active_partials.forEach(partial => {
+    //   let views = (partial.constructor as typeof Partial).__views__
+
+    //   for (let x in views) {
+    //     if (!(x in result)) {
+    //       let types = Reflect.getMetadata('design:paramtypes', views, x)
+    //       result[x] = views[x].bind(null, partial)
+    //     }
+    //   }
+    // });
+
+    return result
   }
 
   /**
    *
    */
-  part(partial: Constructor<Partial>) {
+  go(s: string, params?: Params): Thenable<any>;
+  go(s: Screen, params?: Params): Thenable<any>;
 
-    let part = this.active_partials.get(partial)
+  go(p: Screen|string, params?: Params): Thenable<any> {
 
-    if (part.needsReinit(this.params)) {
-      part = new partial(this)
-    }
+    this.trigger('change:before')
 
-    // Update the next partials with the one we resolved.
-    this.next_partials.set(partial, part)
+    this.activating = true
+    // this.activating_partials = new Map<Constructor<Partial>, Partial>()
 
-    return part
+    return null
   }
+
+  ////////////////////////////////////////////////////////////////////////////
+  /// DECORATORS
+  ////////////////////////////////////////////////////////////////////////////
 
   /**
    * Registers a partial with its name to be able to call it later on.
    */
-  register(p: Constructor<Partial>): void;
-  register(name: string): (p: Constructor<Partial>) => void;
-
-  register(p: Constructor<Partial>|string): any {
+  register(p: Screen): void;
+  register(name: string): (p: Screen) => void;
+  register(p: Screen|string): any {
 
     if (typeof p === 'string') {
-      return (p2: Constructor<Partial>) => {
-        this.registered_partials[p] = p2
+      return (p2: Screen) => {
+
       }
     } else {
       let name = (p as any).name
-      if (name in this.registered_partials)
-        throw new Error(`the partial '${name}' is already registered`)
-      this.registered_partials[name] = p
+      // if (name in this.registered_partials)
+      //   throw new Error(`the partial '${name}' is already registered`)
+      // this.registered_partials[name] = p
     }
 
-  }
-
-  views(V: typeof Views) {
-    return (p: typeof Partial) => {
-      p.__views__ = new V
-    }
   }
 
   params(...props: string[]) {
-    return function decorate(p: typeof Partial) {
+    return function decorate(p: typeof Service) {
       p.__params_check__ = props
     }
-  }
-
-  /**
-   *
-   */
-  go(p: string, params?: Params): Thenable<Partial>;
-  go(p: Constructor<Partial>, params?: Params): Thenable<Partial>;
-
-  go(p: Constructor<Partial>|string, params?: Params): Thenable<Partial> {
-
-    this.trigger('change:before')
-
-    return null
   }
 
 }
@@ -101,13 +126,16 @@ export class App extends Eventable {
 export var app = new App
 
 
-export class Partial extends Eventable {
+/**
+ *
+ */
+export class Service extends Eventable {
 
   static __params_check__: string[]
-  static __views__: Views<Partial>
 
   app: App
   params: Params
+  __requires__: Array<Service> = []
 
   constructor(app: App, params: Params) {
     super()
@@ -122,12 +150,21 @@ export class Partial extends Eventable {
     return null
   }
 
+  public require<P extends Service>(p: Constructor<P>): P {
+    let part = this.app.service(p, this.params)
+    this.__requires__.push(part)
+    return part as P
+  }
+
   /**
    * Override this method to tell when this partial needs to be re-inited.
    */
   public needsReinit(params: Params): boolean {
 
-    let chk = (this.constructor as typeof Partial).__params_check__
+    let chk = (this.constructor as typeof Service).__params_check__
+
+    for (let r of this.__requires__)
+      if (r.needsReinit(params)) return true
 
     if (chk && chk.length) {
       for (let c of chk) {
@@ -139,4 +176,83 @@ export class Partial extends Eventable {
     return false
   }
 
+}
+
+
+export type ViewMap = Map<Block, View>
+
+
+export class Screen {
+
+  public app: App
+  public map: ViewMap
+
+  constructor(app: App, init: ViewMap = null) {
+    this.app = app
+
+    if (init !== null)
+      this.map = new Map(init) as ViewMap
+    else
+      this.map = new Map() as ViewMap
+  }
+
+  include(def: Screen): Screen {
+    return this
+  }
+
+  setBlock(v: Block, view: View) {
+    return this
+  }
+
+}
+
+
+/**
+ *
+ */
+export class View {
+
+  public app: App
+  public deps: Constructor<Service>[]
+  public fn: (...a: Service[]) => Atom
+
+  constructor(app: App) {
+    this.app = app
+  }
+
+}
+
+export class Block {
+
+  public app: App
+
+  constructor(app: App) {
+    this.app = app
+  }
+
+  /** */
+  asAtom(): Atom {
+    return null
+  }
+
+  view<A extends Service, B extends Service, C extends Service, D extends Service, E extends Service, F extends Service>(a: Constructor<A>, b: Constructor<B>, c: Constructor<C>, d: Constructor<D>, e: Constructor<E>, f: Constructor<F>, fn: (a: A, b: B, c: C, d: D, e: E, f: F) => Atom): View;
+  view<A extends Service, B extends Service, C extends Service, D extends Service, E extends Service>(a: Constructor<A>, b: Constructor<B>, c: Constructor<C>, d: Constructor<D>, e: Constructor<E>, fn: (a: A, b: B, c: C, d: D, e: E) => Atom): View;
+  view<A extends Service, B extends Service, C extends Service, D extends Service>(a: Constructor<A>, b: Constructor<B>, c: Constructor<C>, d: Constructor<D>, fn: (a: A, b: B, c: C, d: D) => Atom): View;
+  view<A extends Service, B extends Service, C extends Service>(a: Constructor<A>, b: Constructor<B>, c: Constructor<C>, fn: (a: A, b: B, c: C) => Atom): View;
+  view<A extends Service, B extends Service>(a: Constructor<A>, b: Constructor<B>, fn: (a: A, b: B) => Atom): View;
+  view<A extends Service>(a: Constructor<A>, fn: (c: A) => Atom): View;
+  view(fn: () => Atom): View;
+  view(...a: any[]): View {
+    let v = new View(this.app)
+    let fn = a[a.length - 1]
+    let services = a.slice(0, a.length - 2)
+    v.fn = fn
+    v.deps = services
+    return v
+  }
+
+}
+
+export function DisplayBlock(holder: Block): Atom {
+  return null
 }
